47f9d281bd8b967c8ed071f5d9fcf5a698ec4fa9
jcloud3 <john.cloud3@gmail.com>
2022-11-07 09:44:25 -0600

    Spawn manager implemented with spawn points in training ring

---
+++ Assets/EnemySpawn.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawn : MonoBehaviour
+{
+    public Transform[] spawnPoints;
+    public GameObject[] enemies;
+ 
+    public int startSpawnTime = 10;
+    public int spawnTime = 5;
+ 
+ 
+    // Use this for initialization
+    void Start () {
+        // Call the Spawn function after a delay of the spawnTime and then continue to call after the same amount of time.
+        InvokeRepeating ("Spawn", startSpawnTime, spawnTime);
+    }
+ 
+    // Update is called once per frame
+    void Update () {
+     
+    }
+ 
+    void Spawn () {
+        // Find a random index between zero and one less than the number of spawn points.
+        int spawnPoint = Random.Range (0, 5);
+        int randomEnemy = Random.Range (0, 1);
+        Debug.Log("Spawn");
+        // Create an instance of the enemy prefab at the randomly selected spawn point's position and rotation.
+        Instantiate(enemies[randomEnemy],spawnPoints[spawnPoint].position, spawnPoints[spawnPoint].rotation);
+    }
+ 
+}
