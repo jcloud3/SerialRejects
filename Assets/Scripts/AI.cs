e427c198f5b793bae9f3a8d0c0c886ae5d33d82f
jcloud3 <john.cloud3@gmail.com>
2022-12-13 08:45:01 -0600

    Enemies spawning now pickup players as target for AI

--- Assets/Scripts/AI.cs
+++ Assets/Scripts/AI.cs
@@ -5,14 +5,14 @@ using UnityEngine;
 public class AI : MonoBehaviour
 {
     Animator anim;
-    public Transform player;
+    public GameObject[] player;
     State currentState;
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = GameObject.FindGameObjectsWithTag("Player");
         anim = this.GetComponentInChildren(typeof(Animator)) as Animator;
-        currentState = new Idle(this.gameObject,anim,player);
+        currentState = new Idle(this.gameObject,anim,player[0].transform);
     }
 
     // Update is called once per frame
